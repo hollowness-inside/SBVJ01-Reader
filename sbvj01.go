ea3a8fefa92817ade845a0aea1a3f65b7abe4938
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-05-14 17:03:40 +1000

    Added version parsing

--- sbvj01.go
+++ sbvj01.go
@@ -2,6 +2,7 @@ package sbvj01
 
 import (
 	"bufio"
+	"encoding/binary"
 	"errors"
 	"os"
 )
@@ -37,4 +38,35 @@ func ReadSBVJ01File(path string) (*SBVJ01, error) {
 	if string(magic) != "SBVJ01" {
 		return nil, errors.New("this is not a sbvj01 file")
 	}
+
+	sbvj := new(SBVJ01)
+
+	sbvj.Name = readString(reader)
+
+	versioned, _ := reader.ReadByte()
+	if versioned == 0 {
+		sbvj.Versioned = false
+	} else {
+		sbvj.Versioned = true
+		binary.Read(reader, binary.LittleEndian, &sbvj.Version)
+	}
+}
+
+func readString(r *bufio.Reader) string {
+	return string(readBytes(r))
+}
+
+func readBytes(r *bufio.Reader) []byte {
+	size := readVarint(r)
+
+	bytes := make([]byte, size)
+	r.Read(bytes)
+
+	return bytes
+}
+
+func readVarint(r *bufio.Reader) int {
+	// TODO: Supposed to read VLQ, not a byte
+	v, _ := r.ReadByte()
+	return int(v)
 }
