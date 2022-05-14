b3155f4d2c28e3d05acba3ad54277752d004fe8a
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-05-14 17:41:26 +1000

    String representation of structures added

--- sbvj01token.go
+++ sbvj01token.go
@@ -1,6 +1,31 @@
 package sbvj01
 
+import "fmt"
+
 type SBVJ01Token struct {
 	Type  byte
 	Value any
+}
+
+func (t SBVJ01Token) String() string {
+	if t.Type == NIL {
+		return "NIL"
+	}
+
+	var typeStr string
+	switch t.Type {
+	case DOUBLE:
+		typeStr = "DOUBLE"
+	case BOOLEAN:
+		typeStr = "BOOLEAN"
+	case VARINT:
+		typeStr = "VARINT"
+	case STRING:
+		typeStr = "STRING"
+	case LIST:
+		typeStr = "LIST"
+	case MAP:
+		typeStr = "MAP"
+	}
+	return fmt.Sprintf("%s(%v)", typeStr, t.Value)
 }
