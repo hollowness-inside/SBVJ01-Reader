87a33e8d2bd5246df2c5e41a358dd2fbfc0d8208
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-05-14 16:57:50 +1000

    File reading function created

--- sbvj01.go
+++ sbvj01.go
@@ -1,5 +1,10 @@
 package sbvj01
 
+import (
+	"bufio"
+	"os"
+)
+
 const (
 	NIL     = 0x01
 	DOUBLE  = 0x02
@@ -9,3 +14,19 @@ const (
 	LIST    = 0x06
 	MAP     = 0x07
 )
+
+type SBVJ01 struct {
+	Name      string
+	Versioned bool
+	Version   int32
+}
+
+func ReadSBVJ01File(path string) (*SBVJ01, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+}
