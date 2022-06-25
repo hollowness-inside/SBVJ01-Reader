19d8b9d865694a4c768506b346962eca6548667a
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-06-26 03:45:09 +1000

    Update main.go

--- example/main.go
+++ example/main.go
@@ -10,6 +10,5 @@ func main() {
 
 	fmt.Println("Name:", data.Name)
 	fmt.Printf("Versioned (%t) = %d\n", data.Versioned, data.Version)
-	fmt.Printf("Type: %d\n", data.Value.Type)
 	fmt.Println(data.Value)
 }
