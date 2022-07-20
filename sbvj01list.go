0d3288b8395226dd3a4a378df572dfbfe425f346
MrPythoneer <malikov.vp.dvfu@gmail.com>
2022-07-21 00:36:20 +1000

    Functions rearranged

--- sbvj01list.go
+++ sbvj01list.go
@@ -9,6 +9,10 @@ type SBVJ01List struct {
 	Items []SBVJ01Token
 }
 
+func (l *SBVJ01List) Get(n int) *SBVJ01Token {
+	return &l.Items[n]
+}
+
 func (l SBVJ01List) String() string {
 	strs := make([]string, len(l.Items))
 	for i, v := range l.Items {
@@ -16,8 +20,4 @@ func (l SBVJ01List) String() string {
 	}
 
 	return fmt.Sprintf("[%s]", strings.Join(strs, ", "))
-}
-
-func (l *SBVJ01List) Get(n int) *SBVJ01Token {
-	return &l.Items[n]
 }
--- sbvj01map.go
+++ sbvj01map.go
@@ -9,16 +9,6 @@ type SBVJ01Map struct {
 	Items []SBVJ01Pair
 }
 
-func (m SBVJ01Map) String() string {
-	elements := make([]string, len(m.Items))
-
-	for i, v := range m.Items {
-		elements[i] = v.String()
-	}
-
-	return fmt.Sprintf("{%s}", strings.Join(elements, ", "))
-}
-
 func (m *SBVJ01Map) Get(key string) *SBVJ01Token {
 	for _, it := range m.Items {
 		if it.Key == key {
@@ -27,4 +17,14 @@ func (m *SBVJ01Map) Get(key string) *SBVJ01Token {
 	}
 
 	return nil
+}
+
+func (m SBVJ01Map) String() string {
+	elements := make([]string, len(m.Items))
+
+	for i, v := range m.Items {
+		elements[i] = v.String()
+	}
+
+	return fmt.Sprintf("{%s}", strings.Join(elements, ", "))
 }
